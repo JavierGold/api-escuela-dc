ffcd3aa33768b925d4107f7b6f3125bcb2ca3048
JavierGold <567498jav@gmail.com>
2022-11-28 09:54:54 -0600

    fix: login maestro

--- routes/maestro.js
+++ routes/maestro.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router = express.Router();
 const maestroController = require('../controllers/maestroController')
 
-router.get('/login/:email/:password',maestroController.getMaestroByPasswordAndEmail);
+router.get('/login/:nombreUsuario/:password',maestroController.getMaestroByPasswordAndEmail);
 router.get('/',maestroController.getMaestros);
 router.get('/:id',maestroController.getMaestroById);
 router.post('/',maestroController.post);
